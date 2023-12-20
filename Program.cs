067ad243dee0ac0f2bfe18e4cf66412175f960a9
RysiekR <wrkusmirczak@gmail.com>
2023-12-20 13:45:12 +0100

    main

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello, World!");
+int[,] LD = { { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 } };
+int tw = 5;
+for (int t = 0; t < 2; t++)
+{
+    for (int i = 0; i < tw; i++)
+    {
+        for (int j = 0; j < tw; j++)
+        {
+            Console.Write(LD[i, j]);
+
+            if(LD[i,j]==2)
+            {
+                LD[i,j]=3;
+            }
+        }
+        Console.WriteLine("");
+
+    }
+    Console.WriteLine("");
+
+}
+Console.ReadLine();

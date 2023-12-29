b6d2b967557bdb5b82e0c279a4fe4b5ea55c4002
RysiekR <wrkusmirczak@gmail.com>
2023-12-29 09:00:01 +0100

    GOL In 2d Array xbyx size

--- Program.cs
+++ Program.cs
@@ -1,25 +1,92 @@
 ﻿Console.WriteLine("Hello, World!");
-int[,] LD = { { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 } };
-int tw = 5;
-for (int t = 0; t < 2; t++)
-{
-    for (int i = 0; i < tw; i++)
-    {
-        for (int j = 0; j < tw; j++)
-        {
-            int wynik = LD[i,j];
-            if((i>=1 && i<4) && (j>=1 && j<4))
-            {
+int x = 32;
+int[,] tablica = new int[x, x];
+int[,] tablicaKopia = new int[x, x];
+int wynik = 0;
 
-                wynik = (LD[i-1,j-1]+LD[i-1,j]+LD[i-1,j+1]+LD[i,j-1]+LD[i,j]+LD[i,j+1]+LD[i+1,j-1]+LD[i+1,j]+LD[i+1,j+1]);
-            }
-            LD[i,j]= wynik;
-            Console.Write(LD[i,j] + "  ");
-        }
-        Console.WriteLine("");
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < x; j++)
+    {
+        Random rnd = new Random();
+        int dayNight = rnd.Next(0, 2);
+        tablica[i, j] = dayNight;
 
     }
-    Console.WriteLine(".");
-
 }
-Console.ReadLine();
+
+while (true)
+{
+    int tw = x;
+    {
+        for (int i = 0; i < tw; i++)
+        {
+            for (int j = 0; j < tw; j++)
+            {
+                for (int calcOne = -1; calcOne < 2; calcOne++)
+                {
+                    for (int calcTwo = -1; calcTwo < 2; calcTwo++)
+                    {
+                        int neighbourOne = i + calcOne;
+                        int neighbourTwo = j + calcTwo;
+
+                        if (neighbourOne < 0) { neighbourOne = x - 1; }
+                        if (neighbourTwo < 0) { neighbourTwo = x - 1; }
+                        if (neighbourOne >= x) { neighbourOne = 0; }
+                        if (neighbourTwo >= x) { neighbourTwo = 0; }
+
+                        if (!(calcOne == 0 && calcTwo == 0))
+                        {
+                            wynik = wynik + tablica[neighbourOne, neighbourTwo];
+                        }
+
+                    }
+                }
+
+                if (tablica[i, j] == 1)
+                {
+                    if (wynik == 2 || wynik == 3)
+                    {
+                        wynik = 1;
+                    }
+                    else
+                    {
+                        wynik = 0;
+                    }
+                }
+                else
+                {
+                    if (wynik == 3)
+                    {
+                        wynik = 1;
+                    }
+                    else
+                    {
+                        wynik = 0;
+                    }
+                }
+                tablicaKopia[i, j] = wynik;
+                Console.Write(tablicaKopia[i, j] + "  ");
+                wynik = 0;
+            }
+            Console.WriteLine("");
+
+        }
+        Console.WriteLine("Pause");
+
+    }
+
+    // for (int ii = 0; ii < tw; ii++)
+    // {
+    //     for (int jj = 0; jj < tw; jj++)
+    //     {
+    //         Console.Write(tablica[ii, jj] + "  ");
+    //     }
+    //     Console.WriteLine("");
+
+    // }
+    tablica = (int[,])tablicaKopia.Clone();
+
+    Console.ReadLine();
+}
+

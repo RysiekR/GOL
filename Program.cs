ad7682034323ff4201f86d6355d21be260a3192e
RysiekR <wrkusmirczak@gmail.com>
2023-12-29 13:11:18 +0100

    chance procent pop and restart loop

--- Program.cs
+++ Program.cs
@@ -3,92 +3,135 @@ int x = 32;
 int[,] tablica = new int[x, x];
 int[,] tablicaKopia = new int[x, x];
 int wynik = 0;
-
-for (int i = 0; i < x; i++)
-{
-    for (int j = 0; j < x; j++)
-    {
-        Random rnd = new Random();
-        int dayNight = rnd.Next(0, 2);
-        tablica[i, j] = dayNight;
-
-    }
-}
-
+int population = 0;
+int maxPopulation = x * x;
 while (true)
 {
-    int tw = x;
+
+    Console.WriteLine("What chance to populate you want?(type using numbers 1-100)");
+    string? percentstr = Console.ReadLine()?.ToLower().Trim();
+    int percent = Convert.ToInt32(percentstr);
+    bool playbool = true;
+    for (int i = 0; i < x; i++)
     {
-        for (int i = 0; i < tw; i++)
+        for (int j = 0; j < x; j++)
         {
-            for (int j = 0; j < tw; j++)
+            Random rnd = new Random();
+            int dayNight = rnd.Next(1, 101);
+
+            if (dayNight < percent)
             {
-                for (int calcOne = -1; calcOne < 2; calcOne++)
-                {
-                    for (int calcTwo = -1; calcTwo < 2; calcTwo++)
-                    {
-                        int neighbourOne = i + calcOne;
-                        int neighbourTwo = j + calcTwo;
-
-                        if (neighbourOne < 0) { neighbourOne = x - 1; }
-                        if (neighbourTwo < 0) { neighbourTwo = x - 1; }
-                        if (neighbourOne >= x) { neighbourOne = 0; }
-                        if (neighbourTwo >= x) { neighbourTwo = 0; }
-
-                        if (!(calcOne == 0 && calcTwo == 0))
-                        {
-                            wynik = wynik + tablica[neighbourOne, neighbourTwo];
-                        }
-
-                    }
-                }
-
-                if (tablica[i, j] == 1)
-                {
-                    if (wynik == 2 || wynik == 3)
-                    {
-                        wynik = 1;
-                    }
-                    else
-                    {
-                        wynik = 0;
-                    }
-                }
-                else
-                {
-                    if (wynik == 3)
-                    {
-                        wynik = 1;
-                    }
-                    else
-                    {
-                        wynik = 0;
-                    }
-                }
-                tablicaKopia[i, j] = wynik;
-                Console.Write(tablicaKopia[i, j] + "  ");
-                wynik = 0;
+                dayNight = 1;
             }
-            Console.WriteLine("");
+            else
+            {
+                dayNight = 0;
+            }
+
+            tablica[i, j] = dayNight;
 
         }
-        Console.WriteLine("Pause");
+    }
+    Console.WriteLine("First Population:");
 
+    int tw = x;
+    for (int ii = 0; ii < tw; ii++)
+    {
+        for (int jj = 0; jj < tw; jj++)
+        {
+            Console.Write(tablica[ii, jj] + "  ");
+            if (tablica[ii, jj] == 1)
+            {
+                population++;
+            }
+
+        }
+
+        Console.WriteLine("");
+    }
+    float procPopulation = ((population * 100) / (maxPopulation));
+    Console.WriteLine($"Population: {population} ({procPopulation}%) / Max Population:{maxPopulation}");
+    population = 0;
+    Console.WriteLine("");
+
+    while (playbool)
+    {
+        {
+            for (int i = 0; i < tw; i++)
+            {
+                for (int j = 0; j < tw; j++)
+                {
+                    for (int calcOne = -1; calcOne < 2; calcOne++)
+                    {
+                        for (int calcTwo = -1; calcTwo < 2; calcTwo++)
+                        {
+                            int neighbourOne = i + calcOne;
+                            int neighbourTwo = j + calcTwo;
+
+                            if (neighbourOne < 0) { neighbourOne = x - 1; }
+                            if (neighbourTwo < 0) { neighbourTwo = x - 1; }
+                            if (neighbourOne >= x) { neighbourOne = 0; }
+                            if (neighbourTwo >= x) { neighbourTwo = 0; }
+
+                            if (!(calcOne == 0 && calcTwo == 0))
+                            {
+                                wynik = wynik + tablica[neighbourOne, neighbourTwo];
+                            }
+
+                        }
+                    }
+
+                    if (tablica[i, j] == 1)
+                    {
+                        if (wynik == 2 || wynik == 3)
+                        {
+                            wynik = 1;
+                        }
+                        else
+                        {
+                            wynik = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (wynik == 3)
+                        {
+                            wynik = 1;
+                        }
+                        else
+                        {
+                            wynik = 0;
+                        }
+                    }
+                    if (wynik == 1)
+                    {
+                        population++;
+                    }
+                    tablicaKopia[i, j] = wynik;
+                    Console.Write(tablicaKopia[i, j] + "  ");
+                    wynik = 0;
+                }
+                Console.WriteLine("");
+
+            }
+            procPopulation = ((population * 100) / (maxPopulation));
+            Console.WriteLine($"Population: {population} ({procPopulation}%) / Max Population:{maxPopulation}");
+            population = 0;
+
+        }
+
+        tablica = (int[,])tablicaKopia.Clone();
+
+        Console.WriteLine("Q to restart");
+        string? quit = Console.ReadLine()?.ToLower().Trim();
+
+        if (quit == "q")
+        {
+            playbool = false;
+        }
     }
 
-    // for (int ii = 0; ii < tw; ii++)
-    // {
-    //     for (int jj = 0; jj < tw; jj++)
-    //     {
-    //         Console.Write(tablica[ii, jj] + "  ");
-    //     }
-    //     Console.WriteLine("");
+    // void DrawRectangle (System.Drawing.Pen pen, int 0, int 0, int 100, int 100){};
 
-    // }
-    tablica = (int[,])tablicaKopia.Clone();
 
-    Console.ReadLine();
 }
-
-// void DrawRectangle (System.Drawing.Pen pen, int 0, int 0, int 100, int 100){};
-
